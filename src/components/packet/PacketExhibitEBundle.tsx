f37d205e73aac7f031355cb192cd78ef68906f30
David Arrizon <arrizon.david@gmail.com>
2026-05-16 20:21:04 -0700

    feat: add Exhibit E Legal Bundle component

---
+++ src/components/packet/PacketExhibitEBundle.tsx
@@ -0,0 +1,59 @@
+import { PacketNoticeOfHearing } from './PacketNoticeOfHearing';
+import { PacketNoticeOfViolation } from './PacketNoticeOfViolation';
+import { PacketServiceLog } from './PacketServiceLog';
+
+type Props = {
+  complaint: any;
+  location: any;
+  packet: any;
+  inspector: any;
+  inspections: any;
+  serviceLog: any[];
+};
+
+/**
+ * PacketExhibitEBundle.tsx
+ * 
+ * Groups legal and service documentation into a single Exhibit E bundle.
+ * Sequence: Notice of Hearing (Labeled E) -> NOV -> Proof of Service.
+ */
+export function PacketExhibitEBundle({ 
+  complaint, 
+  location, 
+  packet, 
+  inspector, 
+  inspections,
+  serviceLog 
+}: Props) {
+  return (
+    <div className="exhibit-e-bundle">
+      {/* 1. Notice of Hearing (The Exhibit E Anchor) */}
+      <PacketNoticeOfHearing 
+        complaint={complaint} 
+        location={location} 
+        packet={packet} 
+        inspector={inspector} 
+        inspections={inspections}
+        exhibitLetter="E" 
+      />
+      
+      {/* 2. Notice of Violation (Part of Exhibit E) */}
+      <PacketNoticeOfViolation 
+        complaint={complaint} 
+        location={location} 
+        packet={packet} 
+        inspections={inspections}
+        inspector={inspector}
+      />
+      
+      {/* 3. Proof of Service */}
+      <PacketServiceLog 
+        serviceLog={serviceLog || []}
+        complaint={complaint}
+        location={location}
+        packet={packet}
+        inspector={inspector}
+      />
+    </div>
+  );
+}
